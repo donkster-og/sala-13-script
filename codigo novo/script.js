c04ce69fd07624f1448480beb138a00649023649
donkster-og <rafinhasupersupremo@gmail.com>
2026-04-30 09:30:47 -0300

    Add files via upload

--- codigo novo/script.js
+++ codigo novo/script.js
@@ -1,5 +1,17 @@
 function somar(){
     var n1 = document.getElementById("n1").valueAsNumber
     var n2 = document.getElementById("n2").valueAsNumber 
-    var resp = document.getElementById("resposta")
+    var resp = document.getElementById("respostaSOM")
     resp.textContent = n1 + n2}
+
+    function somar(){
+    var n1 = parseFloat(document.getElementById("n3").value)
+    var n2 = parseFloat(document.getElementById("n4").value)
+    var resp = document.getElementById("respostaDIV")
+
+    if(n4 !==0){
+        resp.textContent = n3 / n4
+    }else{
+        resp.textContent = "Não se divide por 0."
+    }
+}
