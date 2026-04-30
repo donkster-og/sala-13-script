9f07b964764d3e6ed986acedf1b52b3f0c208efc
donkster-og <rafinhasupersupremo@gmail.com>
2026-04-30 09:29:08 -0300

    Add files via upload

---
+++ codigo novo/script.js
@@ -0,0 +1,5 @@
+function somar(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("resposta")
+    resp.textContent = n1 + n2}
